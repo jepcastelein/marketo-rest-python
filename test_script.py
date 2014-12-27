8f13727cbfe46e0f85ea8f72c761e33e8ce0801c
Arunim Samat <arunimsamat@gmail.com>
2014-12-27 13:59:40 +0530

    first commit

---
+++ test_script.py
@@ -0,0 +1,8 @@
+from client import MarketoClient
+
+mc = MarketoClient(host = <Host>, 
+                   client_id = <Client_Id>, 
+                   client_secret = <Client_Secret>)
+
+mc.update_lead(lookupField='email', lookupValue='test@test.com', values={'firstName':'Test1','lastName':'Test2'})
+print(mc.get_leads(filtr='email', values='test@test.com'))
