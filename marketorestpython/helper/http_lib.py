074e0e310279ae2d6d2b3cb7b1b0e96b26980d14
jepcastelein <jep@castelein.net>
2016-03-04 10:40:43 -0800

    cleanup and added rate limiter (max 100 calls per 20 seconds)

--- marketorestpython/helper/http_lib.py
+++ marketorestpython/helper/http_lib.py
@@ -5,70 +5,71 @@ import mimetypes
 class HttpLib:
     max_retries = 3
     sleep_duration = 3
+    num_calls_per_second = 5 # can run five times per second at most (at 100/20 rate limit)
 
+    def rate_limited(maxPerSecond):
+        minInterval = 1.0 / float(maxPerSecond)
+        def decorate(func):
+            lastTimeCalled = [0.0]
+            def rateLimitedFunction(*args,**kargs):
+                elapsed = time.clock() - lastTimeCalled[0]
+                leftToWait = minInterval - elapsed
+                if leftToWait>0:
+                    time.sleep(leftToWait)
+                ret = func(*args,**kargs)
+                lastTimeCalled[0] = time.clock()
+                return ret
+            return rateLimitedFunction
+        return decorate
+
+    @rate_limited(num_calls_per_second)
     def get(self, endpoint, args=None, mode=None):
         retries = 0
         while True:
             if retries > self.max_retries:
                 return None
             try:
-                url = endpoint
-                if args:
-                    r = requests.get(url, params=args)
-                else:
-                    r = requests.get(url)
+                r = requests.get(endpoint, params=args)
+                r_json = r.json()
+                # if we still hit the rate limiter, raise an error so the call will be retried
+                if 'success' in r_json:
+                    if r_json['success'] == False:
+                        print(r_json['errors'][0])
+                        if r_json['errors'][0]['code'] == 606:
+                            print('error 606, rate limiter')
+                            raise
                 if mode is 'nojson':
                     return r
                 else:
-                    return r.json()
+                    return r_json
             except Exception as e:
-                print("HTTP Get Exception!!! Retrying.....")
+                print("HTTP Get Exception! Retrying.....")
                 time.sleep(self.sleep_duration)
                 retries += 1
 
-
+    @rate_limited(num_calls_per_second)
     def post(self, endpoint, args, data=None, files=None, filename=None, mode=None):
         retries = 0
         while True:
             if retries > self.max_retries:
                 return None
             try:
-                headers = {'Content-type': 'application/json'}
                 if mode is 'nojsondumps':
                     r = requests.post(endpoint, params=args, data=data)
-                elif mode is 'merge_lead':
-                    r = requests.post(endpoint, params=args, headers=headers)
-                elif data is None and files is None:
-                    r = requests.post(endpoint, params=args, headers=headers)
-                elif data is not None and files is None:
+                elif files is None:
+                    headers = {'Content-type': 'application/json'}
                     r = requests.post(endpoint, params=args, json=data, headers=headers)
-                elif data is None and files is not None:
-                    # removed the headers with JSON content type, because the file is not JSON
-                    # in future try to infer the correct content type based on file extension (create separate function)
-                    if filename is None:
-                        # print("I'm in open files with NO custom name")
-                        headers = {'Content-type': mimetypes.guess_type(files)[0]}
-                        with open(files,'rb') as f:
-                            files = {'file': f}
-                            r = requests.post(endpoint, params=args, files=files, headers=headers)
-                    else:
-                        # print("I'm in open files with custom name")
-                        with open(files,'rb') as f:
-                            files = {filename: f}
-                            r = requests.post(endpoint, params=args, files=files)
-                else:
-                    # removed the headers with JSON content type, because the file is not JSON
-                    # print("I'm in 'else'")
-                    headers = {'Content-type': mimetypes.guess_type(files)[0]}
-                    with open(files,'rb') as f:
-                        files = {'file': f}
-                        r = requests.post(endpoint, params=args, json=data, files=files, headers=headers)
+                elif files is not None:
+                    mimetype = mimetypes.guess_type(files)[0]
+                    file = {filename: (files, open(files, 'rb'), mimetype)}
+                    r = requests.post(endpoint, params=args, json=data, files=file)
                 return r.json()
             except Exception as e:
-                print("HTTP Post Exception!!! Retrying....."+ str(e))
+                print("HTTP Post Exception! Retrying....."+ str(e))
                 time.sleep(self.sleep_duration)
                 retries += 1
 
+    @rate_limited(num_calls_per_second)
     def delete(self, endpoint, args, data):
         retries = 0
         while True:
@@ -79,6 +80,6 @@ class HttpLib:
                 r = requests.delete(endpoint, params=args, json=data, headers=headers)
                 return r.json()
             except Exception as e:
-                print("HTTP Delete Exception!!! Retrying....."+ str(e))
+                print("HTTP Delete Exception! Retrying....."+ str(e))
                 time.sleep(self.sleep_duration)
                 retries += 1
