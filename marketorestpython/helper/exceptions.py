8a179f4c8efdab7b972b6cd5e7be4a803f82309c
Tejas Manohar <me@tejas.io>
2016-07-15 02:37:20 -0700

    fix key error, self.msg

--- marketorestpython/helper/exceptions.py
+++ marketorestpython/helper/exceptions.py
@@ -3,7 +3,7 @@ class MarketoException(Exception):
     message = None
     code = None
     def __init__(self, exc = {'message':None,'code':None}):
-        self.message = exc['message']
+        self.msg = exc['message']
         self.code = exc['code']
       
     def __str__(self):
